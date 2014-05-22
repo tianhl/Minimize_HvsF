ca80fd27b1b2a4ba0c4804f954aea0551cfb827f
tianhl <tianhl@tianhl-c105.(none)>
2014-05-22 12:28:37 +0800

    caculate H vs F

--- OneDimNumMini.cxx
+++ OneDimNumMini.cxx
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <map>
 #include <math.h>
+#include "TGraph.h"
 using namespace std;
 
 
@@ -10,12 +12,6 @@ double H(const double *xx)
   const double K2    = xx[2];
   const double Phi   = xx[3];
   const double Alpha = xx[4];
-
-  std::cout << "M:     " <<  M     << std::endl;
-  std::cout << "K1:    " <<  K1    << std::endl;
-  std::cout << "K2:    " <<  K2    << std::endl;
-  std::cout << "Phi:   " <<  Phi   << std::endl;
-  std::cout << "Alpha: " <<  Alpha << std::endl;
 
   const double Eq1   = -1/(M*sin(Phi-Alpha));
   const double Eq2   = (4*M_PI*M*M-2*K1-4*K2)*cos(Phi)*sin(Phi);
@@ -67,12 +63,39 @@ double F(const double *xx)
   const double M     = xx[0];
   return 1.76*10000000/(2*M_PI*M)*sqrt(H1(xx)*H2(xx));	
 }
+
  
 int main()
 {
-  double xx[5] ={2.0,2.0,2.0,2.0,1.0};
-  std::cout << "H1: " << H1(xx) << std::endl;
-  std::cout << "H2: " << H2(xx) << std::endl;
-  std::cout << "H: "  << H(xx)  << std::endl;
-  std::cout << "F: "  << F(xx)  << std::endl;
+
+  double m     = 1257.;
+  double alpha = 0.1651;
+  double k1    = 5000000.;
+  double k2    = 5000.;
+  double phi   = alpha;
+
+  double xx[5] = {m, k1, k2, phi, alpha};
+  typedef std::map<double, double> HFMap;
+  typename HFMap::iterator hfmap_it;
+  HFMap hfmap;
+  
+  double nstep    = 100;
+  double phi_step = (M_PI/2 - alpha)/nstep;
+  for(int i = 0; i < nstep; i++){
+    double h = H(xx);    
+    double f = F(xx);
+    xx[3]    = xx[3] + phi_step;
+    hfmap.insert(std::pair<double,double>(h,f));
+  }
+
+  for(hfmap_it=hfmap.begin(); hfmap_it != hfmap.end(); hfmap_it++){
+    std::cout << "H vs F: "  << hfmap_it->first  << " " << hfmap_it->second << std::endl;
+  }
+
 }
+
+
+
+
+
+
