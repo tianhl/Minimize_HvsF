0b4fca2f9913ff8cf0a5c001cd4d33cdec6e6659
tianhl <tianhl@tianhl-c105.(none)>
2014-05-26 15:08:34 +0800

    debug formula

--- OneDimNumMini.cxx
+++ OneDimNumMini.cxx
@@ -11,10 +11,6 @@
 #include "TRandom2.h"
 using namespace std;
 
-const double alphalist[8]  = {0.1651,     0.1767,     0.1899,     0.2054,     0.2234,     0.24498,    0.27095,    0.30288   };
-const double dataPointH[8] = {3041.38127, 2844.29253, 2647.64046, 2451.53013, 2256.10283, 2061.55281, 1868.15417, 1676.30546};
-const double dataPointF[8] = {1.57145e10, 1.58135e10, 1.60627e10, 1.60596e10, 1.62094e10, 1.6381e10,  1.68788e10, 1.64925e10};
-
 
 double H(const double *xx)
 {
@@ -140,75 +136,76 @@ double GetPhiFromH(const double *pars, const double hpoint){
   return xs[3];
 }
 
+const double alphalist[8]  = {0.1651,     0.1767,     0.1899,     0.2054,     0.2234,     0.24498,    0.27095,    0.30288   };
+const double dataPointH[8] = {3041.38127, 2844.29253, 2647.64046, 2451.53013, 2256.10283, 2061.55281, 1868.15417, 1676.30546};
+const double dataPointF[8] = {1.57145e10, 1.58135e10, 1.60627e10, 1.60596e10, 1.62094e10, 1.6381e10,  1.68788e10, 1.64925e10};
 
 int main()
 {
-  double chi2 = 0;
 
-  for (int i = 0; i < 8; i++){
-    std::cout << "===================== data point " << i << std::endl;
-    double m      = 1257.;
-    double alpha  = alphalist[i];
-    double k1     = 1e7;
-    double k2     = 5e5;
-    double phi    = alpha;
-    double hpoint = dataPointH[i];
-    double xx[5] = {m, k1, k2, phi, alpha};
+  int i = 0;
+  const double m      = 1257.;
+  const double alpha  = alphalist[i];
+  const double k1     = 1e7;
+  const double k2     = 5e5;
+  const double hpoint = dataPointH[i];
+  double phi    = alpha;
+  double xx[5] = {m, k1, k2, phi, alpha};
 
-    // find H point
-
-
-    const double pars[4] = {m,k1,k2,alpha};
-    phi = GetPhiFromH(pars,hpoint);
-    std::cout << "GetPhiFromH: " << phi << std::endl;
-
-    xx[3] = phi;
-    std::cout << "H point: " << H(xx) << std::endl;
-    std::cout << "F point: " << F(xx) << std::endl;
-    std::cout << "Diff btw Theory and Exp: " << F(xx) - dataPointF[i] << std::endl;
-    double diff = F(xx) - dataPointF[i];
-
-    chi2 += abs(diff);
+//  // find H point
+//
+//
+//  const double pars[4] = {m,k1,k2,alpha};
+//  phi = GetPhiFromH(pars,hpoint);
+//  std::cout << "GetPhiFromH: " << phi << std::endl;
+//
+//  xx[3] = phi;
+//  std::cout << "H point: " << H(xx) << std::endl;
+//  std::cout << "F point: " << F(xx) << std::endl;
+//  std::cout << "Diff btw Theory and Exp: " << F(xx) - dataPointF[i] << std::endl;
+//  double diff = F(xx) - dataPointF[i];
+//
+//
+//  std::cout << "chi2: " << diff << std::endl;
+  // Draw Plot 
+  
+  typedef std::map<double, double> HFMap;
+  typename HFMap::iterator hfmap_it;
+  HFMap hfmap;
+  
+  int Nstep    = 100;
+  double phi_step = (M_PI/2 - alpha)/(Nstep+200);
+  
+  double harray[Nstep];
+  double farray[Nstep];
+  
+  xx[3] = alpha;
+  for(i = 0; i < Nstep; i++){
+    xx[3]    = xx[3] + phi_step;
+    double h, f;    
+    HvsF(xx, &h, &f);
+    hfmap.insert(std::pair<double,double>(h,f));
+    std::cout << "H: " << h << " F: " << f << std::endl;
   }
-
-  std::cout << "chi2: " << chi2 << std::endl;
-  //  // Draw Plot 
-  //
-  //  typedef std::map<double, double> HFMap;
-  //  typename HFMap::iterator hfmap_it;
-  //  HFMap hfmap;
-  //
-  //  const int Nstep    = 1000;
-  //  double phi_step = (M_PI/2 - alpha)/(Nstep+2);
-  //
-  //  double harray[Nstep];
-  //  double farray[Nstep];
-  //
-  //  int i = 0;
-  //  for(i = 0; i < Nstep; i++){
-  //    xx[3]    = xx[3] + phi_step;
-  //    double h, f;    
-  //    HvsF(xx, &h, &f);
-  //    hfmap.insert(std::pair<double,double>(h,f));
-  //    //std::cout << "H: " << h << " F: " << f << std::endl;
-  //  }
-  //
-  //  i = 0;
-  //  for(hfmap_it=hfmap.begin(); hfmap_it != hfmap.end(); hfmap_it++){
-  //    harray[i] = hfmap_it->first;
-  //    farray[i] = hfmap_it->second;
-  //    i++;
-  //  }
-  //
-  //
-  //  TCanvas *cav  = new TCanvas("c1");
-  //  TGraph  *hvsf = new TGraph(Nstep, harray, farray);
-  //  TMarker *data = new TMarker(dataPointH[0], dataPointF[0], 20);
-  //  data->SetMarkerColor(2);
-  //  hvsf->SetMarkerStyle(21);
-  //  hvsf->Draw("APL");
-  //  data->Draw();
-  //  cav->Print("FunGraph.eps");
+  
+  i = 0;
+  for(hfmap_it=hfmap.begin(); hfmap_it != hfmap.end(); hfmap_it++){
+    if (hfmap_it->first >= 10000.) break;
+    harray[i] = hfmap_it->first;
+    farray[i] = hfmap_it->second;
+    i++;
+  }
+  Nstep = i ;
+  
+  
+  TCanvas *cav  = new TCanvas("c1");
+  TGraph  *hvsf = new TGraph(Nstep, harray, farray);
+  TMarker *data = new TMarker(dataPointH[0], dataPointF[0], 20);
+  data->SetMarkerColor(2);
+  hvsf->SetMarkerStyle(21);
+  hvsf->Draw("APL");
+  data->Draw();
+  cav->Print("FunGraph.eps");
 
 
 }
